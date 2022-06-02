78104dc070b6c7441e371f59022109c70472a9ed
mgorem <oremgitonga22@gmail.com>
2022-06-02 15:45:38 +0300

    create banners render homepage

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,53 @@ import Link from "next/link"; //
 import Image from "next/image"; //Optimized version of the basic image component
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
-const Banner = ({purpose}) => (
+const Banner = ({purpose, imageUrl, title1, title2, desc1,desc2, linkName, buttonText}) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-    {purpose}
+    <Image src={imageUrl} width={500} height={300} alt="banner" />
+    <Box p="5">
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
+      <Text fontSize="3xl" fontWeight="bold">{title1} <br /> {title2}</Text>
+      <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">{desc1}<br /> {desc2}</Text>
+      <Button fontSize="xl">
+        <Link href={linkName}>{buttonText}</Link>
+      </Button>
+    </Box>
   </Flex>
 )
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <Banner purpose={'For Sale'}/>
-      <Banner purpose={'For Rent'}/>
-    </div>
+    <Box>
+      <Banner 
+        purpose="RENT A HOME"
+        title1="Rent Homes for"
+        title2="Everyone"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Renting"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
+
+      {/* Fetch properties and map over them... */}
+      <Flex flexWrap="wrap">
+        
+      </Flex>
+      <Banner 
+        purpose="BUY A HOME"
+        title1="Find Buy & Own Your"
+        title2="Dream Home"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Buying"
+        linkName="/search?purpose=for-sale"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+      />
+
+      {/* Fetch properties and map over them... */}
+      <Flex flexWrap="wrap">
+        
+      </Flex>
+    </Box>
   )
 }
---
+++ utils/fetchApi.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const baseUrl = 'https://bayut.p.rapidapi.com'
+
+// headers: {
+//     'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+//     'X-RapidAPI-Key': 'af9641c82amshad0454fd12395a3p169c4ajsn3d6b686fad59'
+//   }
